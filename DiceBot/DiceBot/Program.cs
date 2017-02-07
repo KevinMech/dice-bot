81f732c481646fc6e8e85aff37018aede66b809f
Kevin <KevinMech@users.noreply.github.com>
2017-02-07 08:58:24 -0500

    Created basic outline to connect bot online and test for connection status

--- DiceBot/DiceBot/Program.cs
+++ DiceBot/DiceBot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace DiceBot
 {
@@ -10,6 +11,18 @@ namespace DiceBot
     {
         static void Main(string[] args)
         {
+            DiscordClient dicebot = new DiscordClient();
+            dicebot.Connect(Token.getToken, TokenType.Bot);
+            dicebot.MessageReceived += DebugEchoTest;
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static void DebugEchoTest(object sender, MessageEventArgs e)
+        {
+            e.Channel.SendMessage("Message Received!");
         }
     }
 }
