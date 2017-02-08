f8d46a26d298e230480b8c33b3b8e3c616584891
Kevin <KevinMech@users.noreply.github.com>
2017-02-08 10:27:50 -0500

    Added new method for connecting to Discords server

--- DiceBot/DiceBot/DiceBot.cs
+++ DiceBot/DiceBot/DiceBot.cs
@@ -14,8 +14,48 @@ namespace DiceBot
         public int DiceRolled { get; set; }
         public DiceBot(Action<DiscordConfigBuilder> configFunc) : base(configFunc)
         {
-            Console.WriteLine("Connecting...");
-            ExecuteAndWait(async () => await Connect(Token.getToken, TokenType.Bot));
+            Connect(3, 4000);
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Attempts to connect to Discords Server
+        /// </summary>
+        /// <param name="maxTries">The amount of tries the bot will attempt to connect to the server</param>
+        /// <param name="timeout">The amount of time in milliseconds the bot will make in between each attempt</param>
+        private void Connect(int maxTries, int timeout)
+        {
+            bool success = false;
+            Console.WriteLine("Connecting to server...");
+            for (int tries = 0; tries < maxTries; tries++)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("attempting connection...[" + (tries + 1) + "/3]");
+                    Task task = Task.Run(async () => await Connect(Token.getToken, TokenType.Bot));
+                    task.Wait();
+                    success = true;
+                    Console.WriteLine("Connected to server!");
+                    break;
+                }
+                catch (AggregateException e)
+                {
+                    Console.WriteLine("Failed!");
+                    foreach (Exception exception in e.InnerExceptions) Console.WriteLine(exception.Message);
+                    if(tries < (maxTries - 1)) System.Threading.Thread.Sleep(timeout);
+                }
+            }
+            if (!success)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Could not connect to server!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
         }
 
         /// <summary>
