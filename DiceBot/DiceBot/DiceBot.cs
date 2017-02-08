6e251231d6f01b40f3d77b9361ff37052025b249
Kevin <KevinMech@users.noreply.github.com>
2017-02-08 05:14:36 -0500

    Created a new class DiceBot that inherits from DiscordClient in order to keep track of various statistics and other unique properties planned in the future

---
+++ DiceBot/DiceBot/DiceBot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DiceBot
+{
+    class DiceBot : DiscordClient
+    {
+        //statistics recorded for the bot
+        public int TimesRun { get; }
+        public int DiceRolled { get; set; }
+        public DiceBot(Action<DiscordConfigBuilder> configFunc) : base(configFunc)
+        {
+            Console.WriteLine("Connecting...");
+            ExecuteAndWait(async () => await Connect(Token.getToken, TokenType.Bot));
+        }
+
+        /// <summary>
+        /// Logs any errors encountered by the discord API and prints to console screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void ErrorLogHandling(object sender, LogMessageEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- DiceBot/DiceBot/Program.cs
+++ DiceBot/DiceBot/Program.cs
@@ -11,39 +11,14 @@ namespace DiceBot
     {
         static void Main(string[] args)
         {
-            DiscordClient dicebot = new DiscordClient(metadata =>
+            DiceBot dicebot = new DiceBot(metadata =>
             {
                 metadata.AppName = "DiscordDiceBot";
                 metadata.AppVersion = "0.1.0";
                 metadata.AppUrl = "https://github.com/KevinMech/dice-bot";
                 metadata.LogLevel = LogSeverity.Error;
-                metadata.LogHandler += ErrorLogHandling;
+                metadata.LogHandler += DiceBot.ErrorLogHandling;
             });
-            dicebot.ExecuteAndWait(async () =>
-            {
-                await dicebot.Connect(Token.getToken, TokenType.Bot);
-            });
-            while (true)
-            {
-                Console.ReadLine();
-            }
         }
-
-        /// <summary>
-        /// Logs any errors encountered by the discord API and prints to console screen
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        static void ErrorLogHandling(object sender, LogMessageEventArgs e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(e.Message);
-        }
-
-        //async static void DebugEchoTest(object sender, MessageEventArgs e)
-        //{
-        //    Console.WriteLine("message sent");
-        //    await e.Channel.SendMessage("Message Received!");
-        //}
     }
 }
